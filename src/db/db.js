f83bb85a05767531164238aba020cc70f9a69f75
paliop <135032802+paliop@users.noreply.github.com>
2024-08-23 17:50:54 -0300

    cambio db

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+
 
 
 const firebaseConfig = {
@@ -24,7 +24,7 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 
 const db=getFirestore()
-const auth=getAuth()
-export {db, auth};
+
+export {db};
 
 
